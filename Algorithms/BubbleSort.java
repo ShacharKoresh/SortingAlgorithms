ca03b6c3af2b6072a1305b30547505cf5ae5783b
ShacharKoresh <77329952+ShacharKoresh@users.noreply.github.com>
2021-01-14 17:23:48 +0200

    Update BubbleSort.java

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -8,7 +8,7 @@
 * @param arr - the array to be sorted
 */
 public static void bubbleSort(double[] arr){
-  bubbleSort(arr, 0, arr.length - 1);
+	bubbleSort(arr, 0, arr.length - 1);
 }
 
 
@@ -22,26 +22,26 @@ public static void bubbleSort(double[] arr){
 * @param rightIndex - the most right Index in the subArray
 */
 public static void bubbleSort(double[] arr, int leftIndex, int rightIndex){
-  //if there is at least 1 element
-  if ((rightIndex - leftIndex) >= 1) {
-    double temp = 0;
-    boolean swap = true;
-    for (int i = leftIndex ; i < rightIndex ; i++) {
-      for (int j = leftIndex + 1; j <= rightIndex ; j++) {
-        //if 2 elements are not sorted - swap between them
-        if (arr[j - 1] > arr[j]) {
-          temp = arr[j - 1];
-          arr[j - 1] = arr[j];
-          arr[j] = temp;
-          swap = false;
-        }
-      }
+	//if there is at least 1 element
+	if ((rightIndex - leftIndex) >= 1) {
+  	double temp = 0;
+  	boolean swap = true;
+  	for (int i = leftIndex ; i < rightIndex ; i++) {
+    	for (int j = leftIndex + 1; j <= rightIndex ; j++) {
+      	//if 2 elements are not sorted - swap between them
+      	if (arr[j - 1] > arr[j]) {
+        	temp = arr[j - 1];
+        	arr[j - 1] = arr[j];
+        	arr[j] = temp;
+        	swap = false;
+      	}
+			}
       //if no inversions were made, it means that the array is sorted - function stops.
       if(swap == true) {
-        break;
+      	break;
       }
       else {
-        swap = true;
+     		swap = true;
       }
     }
   }
